ac669e4054a83c8ba1db1dc48968a87273ffdef9
ConfusedKnight <aarushdangol2006@gmail.com>
2025-11-20 16:40:11 +0545

    fix: fixed route paths

--- backend/routes/meal_sessions.py
+++ backend/routes/meal_sessions.py
@@ -7,16 +7,16 @@ import uuid
 from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
 
-router = APIRouter(prefix="/mealsession")
+router = APIRouter(prefix="/meal-session")
 
-@router.post("/create", response_model = MealSessionResponse)
+@router.post("/", response_model = MealSessionResponse)
 def meal_session_create(event_name: str, meal_session_data: MealSessionRegister, db: Session = Depends(get_db)):
     return create_meal_session(event_name, meal_session_data, db)
 
-@router.delete("/delete")
+@router.delete("/{meal_id}")
 def meal_session_delete(meal_id: uuid.UUID, db: Session = Depends(get_db)):
     return delete_meal_session(meal_id, db)
 
-@router.put("/update")
+@router.put("/{meal_id}")
 def meal_session_update(meal_id: uuid.UUID, new_time: MealSessionUpdate, db: Session = Depends(get_db)):
     return update_meal_session(meal_id, new_time, db)
