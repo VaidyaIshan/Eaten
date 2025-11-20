135f0f32c21cca899c90535acc8b85dd485fee81
ConfusedKnight <aarushdangol2006@gmail.com>
2025-11-20 14:56:06 +0545

    fix: fix merge error in main.py

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from db import engine, Base
 from routes.auth import router as auth_router
 from routes.events import router as event_router
 from routes.feedbacks import router as feedback_router
+from routes.meal_sessions import router as meal_session_router
 
 
 Base.metadata.create_all(bind=engine)
@@ -14,4 +15,5 @@ app = FastAPI()
 app.include_router(auth_router)
 app.include_router(event_router)
 app.include_router(feedback_router)
+app.include_router(meal_session_router)
 
