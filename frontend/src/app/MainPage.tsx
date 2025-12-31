723e2150c4d0bf00a6b2505486932354e60c2019
IshanBaidya <165746811+VaidyaIshan@users.noreply.github.com>
2026-01-01 04:39:10 +0545

    Update:Added font Cabinet Grotesk and Setup

--- frontend/src/app/MainPage.tsx
+++ frontend/src/app/MainPage.tsx
@@ -1,7 +1,10 @@
 export default function Home(){
     return(
         <>
-        <h1 className="text-white bg-primary h-128">Eaten</h1>
+   <h1 className="font-normal">Regular</h1>
+<h1 className="font-semibold">Semibold</h1>
+<h1 className="font-bold">Bold</h1>
+<h1 className="font-black">Black</h1>
         </>
     )
 }
