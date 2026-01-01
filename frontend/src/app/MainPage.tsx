46dde3fe33f803d9a765b1378806f35990190c31
IshanBaidya <165746811+VaidyaIshan@users.noreply.github.com>
2026-01-01 06:46:56 +0545

    Add: Completion of mobile view not-found page

--- frontend/src/app/MainPage.tsx
+++ frontend/src/app/MainPage.tsx
@@ -1,10 +1,10 @@
-export default function Home(){
+
+export default function MainPage(){
     return(
         <>
-   <h1 className="font-normal">Regular</h1>
-<h1 className="font-semibold">Semibold</h1>
-<h1 className="font-bold">Bold</h1>
-<h1 className="font-black">Black</h1>
+        <div className="">
+           
+        </div>
         </>
     )
 }
