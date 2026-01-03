363dde1a4013442e6363cad59487a3d63c494b33
IshanBaidya <165746811+VaidyaIshan@users.noreply.github.com>
2026-01-03 20:40:40 +0545

    Changed Landing Page

--- frontend/src/app/MainPage.tsx
+++ frontend/src/app/MainPage.tsx
@@ -1,9 +1,20 @@
-
+import React from "react"
+import DonutSVG from "./assets/vectors/Donut"
+import MoonSVG from "./assets/vectors/Moon"
 export default function MainPage(){
     return(
         <>
-        <div className="">
-           
+        <div className="min-h-screen flex flex-col justify-center left-5 fixed">
+              <div className="fixed right-2 top-4">
+                    <MoonSVG/>
+                </div>
+
+            <p className="text-[96px] font-bold text-white ">Eaten</p>
+            <p className="text-[30px] font-bold text-accent ">Where every bite feels unreal</p>
+
+             <div className="fixed bottom-0 right-0">
+                    <DonutSVG/>
+                 </div>
         </div>
         </>
     )
