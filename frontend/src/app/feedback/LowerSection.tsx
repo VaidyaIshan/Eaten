c9342a3f1a075c49a2c9f48bde900f45cfa7b687
ConfusedKnight <aarushdangol2006@gmail.com>
2026-01-07 19:11:11 +0545

    add: complete frontend mobile view

--- frontend/src/app/feedback/LowerSection.tsx
+++ frontend/src/app/feedback/LowerSection.tsx
@@ -6,7 +6,7 @@ const LowerSection = () => {
 
   return(
     <div className = "absolute top-[13.5rem] bottom-0 left-0 right-0 bg-[#FFC75F] w-full max-4xl pt-[10px] m-0">
-      <div className = "flex flex-col items-center justify-center bg-[#FBF7FF] relative bottom-0 w-full h-full gap-1">
+      <div className = "relative flex flex-col items-center justify-center bg-[#FBF7FF] w-full h-full overflow-y-auto gap-1">
         <p className = "text-[#000000] absolute top-[1rem] left-[1.5rem]">Feedback Form:</p>
         
         <div className = "absolute top-[3rem] left-0 px-[1.5rem] m-0 w-full flex flex-col gap-[2rem]">
--- frontend/src/app/feedback/getAllFeedbacks.tsx
+++ frontend/src/app/feedback/getAllFeedbacks.tsx
@@ -1,7 +1,7 @@
 const GetAllFeedback = () => {
 
   return(
-    <div className = "flex flex-col w-full">
+    <div className = "flex flex-grow flex-col w-full gap-[1rem]">
 
       <div className = "w-full flex flex-col">
         <p className = "text-[#000000] text-[10px]">See what others are saying below!</p>
@@ -15,17 +15,71 @@ const GetAllFeedback = () => {
       </div>
 
       <div className = "w-full flex-col w-full">
-        <div className = "w-full flex flex-row">
-          <div className = "w-[1.125rem] h-[1.125rem] rounded-full bg-[#7466C9] text-center">
-            <p>A</p>
+
+        <div className = "w-full flex flex-row items-center justify-start gap-[5px] pb-[5px]">
+          <div className = "w-[1.125rem] h-[1.125rem] rounded-full bg-[#7466C9] flex text-center items-center justify-center">
+            <p className = "text-[10px]">A</p>
           </div>
+
+          <p className = "text-[#000000] text-[10px]">@AryanShahi</p>
         </div>
+
+        <div className = "bg-[#F3F3F3] flex text-center items-center justify-start h-[1.125rem] p-[3px]">
+          <p className = "text-[#000000] text-[10px]">
+            &gt; Eaten.
+          </p>
+        </div>
+
       </div>
 
+
+      <div className = "w-full flex-col w-full">
+
+        <div className = "w-full flex flex-row items-center justify-start gap-[5px] pb-[5px]">
+          <div className = "w-[1.125rem] h-[1.125rem] rounded-full bg-[#7466C9] flex text-center items-center justify-center">
+            <p className = "text-[10px]">A</p>
+          </div>
+
+          <p className = "text-[#000000] text-[10px]">@Azrael</p>
+        </div>
+
+        <div className = "bg-[#F3F3F3] flex text-center items-center justify-start h-[1.125rem] p-[3px]">
+          <p className = "text-[#000000] text-[10px]">
+            &gt; I feel quite hungry.
+          </p>
+        </div>
+
+      </div>
+
+      <div className = "w-full flex-col w-full">
+
+        <div className = "w-full flex flex-row items-center justify-start gap-[5px] pb-[5px]">
+          <div className = "w-[1.125rem] h-[1.125rem] rounded-full bg-[#7466C9] flex text-center items-center justify-center">
+            <p className = "text-[10px]">D</p>
+          </div>
+
+          <p className = "text-[#000000] text-[10px]">@DevilPhantom</p>
+        </div>
+
+        <div className = "bg-[#F3F3F3] flex text-center items-center justify-start h-[1.125rem] p-[3px]">
+          <p className = "text-[#000000] text-[10px]">
+            &gt; Idk here is some lorem text "Lorem ipsum dolor sit...
+          </p>
+        </div>
+
+      </div>
+
+      <div className = "w-full flex flex-col items-center">
+        <div className = "w-full h-[1px] bg-[#EFECEC] flex justify-end">
+          <div className = "hidden">
+              <p>placeholder</p>
+          </div>
+        </div>
+          <p className = "text-[#CCCCCC] text-[10px] italic">That's all for now...</p>
+      </div>
     </div>
   )
 
 }
 
 export default GetAllFeedback;
-
