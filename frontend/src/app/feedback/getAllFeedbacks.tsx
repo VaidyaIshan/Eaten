681862fa487f161bbe30c5c4742e18008cf16773
hardikshakya-prog <hardik.shakya@deerwalk.edu.np>
2026-01-08 11:24:24 +0545

    Updated with main and aligned the line design in feedback

--- frontend/src/app/feedback/getAllFeedbacks.tsx
+++ frontend/src/app/feedback/getAllFeedbacks.tsx
@@ -5,10 +5,10 @@ const GetAllFeedback = () => {
 
       <div className = "w-full flex flex-col">
         <p className = "text-[#000000] text-[10px]">See what others are saying below!</p>
-        <div className = "w-full h-[2px] bg-[#7466C9] flex justify-end">
-          <div className = "relative w-[3.75rem] h-[5px] bg-[#7466C9] rounded-[9px] bottom-[1px] right-[10px]">
+        <div className = "w-full h-[2px] bg-[#7466C9] flex justify-end items-center rounded-sm">
+          <div className = "relative w-[3.75rem] h-[5px] bg-[#7466C9] rounded-[9px] right-[10px]">
             <div className = "hidden">
-              <p>placeholder</p>
+              {/* <p>placeholder</p> */}
             </div>
           </div>
         </div>
