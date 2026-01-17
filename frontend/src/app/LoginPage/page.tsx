e9cb32f56387742fecd050a1d6e73c6782584e9c
ConfusedKnight <aarushdangol2006@gmail.com>
2026-01-17 23:47:06 +0545

    fix: fixed mobile view

--- frontend/src/app/LoginPage/page.tsx
+++ frontend/src/app/LoginPage/page.tsx
@@ -3,17 +3,25 @@ import LoginSection from "../components/LoginSection/login-section"
 export default function LoginPage(){
   return (
     <>
-      <div className="fixed right-[8px] top-[2rem] z-50">
-        <MoonSVG />
+ <div className="min-h-screen w-full flex flex-col items-center relative overflow-hidden">
+      
+      <div className="w-full flex-none pt-12 pb-10 flex flex-col items-center relative z-10">
+        
+
+        <div className="mt-16 mb-4">
+          <h1 className="text-white text-[70px] font-bold leading-none">
+            Eaten
+          </h1>
+        </div>
       </div>
 
-      <div className="min-h-screen flex flex-col justify-evenly items-center px-4">
-        <p className="font-bold text-[80px] leading-none text-center">
-          Eaten
-        </p>
-
-        <LoginSection />
+      <div className="w-full flex-1 flex flex-col items-center">
+        <div className="w-full max-w-xs">
+          <LoginSection />
+        </div>
+        
       </div>
+    </div>
     </>
   )
 }
